6ff2c87f107746831bb6014edbda6b35c98f8654
378978764 <378978764@qq.com>
2021-01-15 09:54:51 +0800

    [A] 仅仅对 matlab 文件提出诊断.

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -15,11 +15,15 @@ const SOURCE = 'Matlab Comment Checker'
  * Analyzes the text document for problems. 
  * This demo diagnostic problem provider finds all mentions of 'emoji'.
  * @param doc text document to analyze
- * @param emojiDiagnostics diagnostic collection
+ * @param matlabDiagnostics diagnostic collection
  */
-export function refreshDiagnostics(doc: vscode.TextDocument, emojiDiagnostics: vscode.DiagnosticCollection): void {
-	const diagnostics: vscode.Diagnostic[] = [];
+export function refreshDiagnostics(doc: vscode.TextDocument, matlabDiagnostics: vscode.DiagnosticCollection): void {
+	// 仅仅对 matlab 文件提出诊断
+	if (doc.languageId !== 'matlab') {
+		return
+	}
 
+	const diagnostics: vscode.Diagnostic[] = [];
 	// 代码主体中的变量
 	const variables = extractVariables(doc.getText())
 	for (let variable of variables) {
@@ -51,27 +55,31 @@ export function refreshDiagnostics(doc: vscode.TextDocument, emojiDiagnostics: v
 		})
 	}
 
-	emojiDiagnostics.set(doc.uri, diagnostics);
+	matlabDiagnostics.set(doc.uri, diagnostics);
 }
 
-export function subscribeToDocumentChanges(context: vscode.ExtensionContext, emojiDiagnostics: vscode.DiagnosticCollection): void {
+export function subscribeToDocumentChanges(context: vscode.ExtensionContext, matlabDiagnostics: vscode.DiagnosticCollection): void {
+	// 刚开始
 	if (vscode.window.activeTextEditor) {
-		refreshDiagnostics(vscode.window.activeTextEditor.document, emojiDiagnostics);
+		refreshDiagnostics(vscode.window.activeTextEditor.document, matlabDiagnostics);
 	}
+
+	// 活动编辑器改变
 	context.subscriptions.push(
 		vscode.window.onDidChangeActiveTextEditor(editor => {
 			if (editor) {
-				refreshDiagnostics(editor.document, emojiDiagnostics);
+				refreshDiagnostics(editor.document, matlabDiagnostics);
 			}
 		})
 	);
 
+	// 文档改变
 	context.subscriptions.push(
-		vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document, emojiDiagnostics))
+		vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document, matlabDiagnostics))
 	);
 
 	context.subscriptions.push(
-		vscode.workspace.onDidCloseTextDocument(doc => emojiDiagnostics.delete(doc.uri))
+		vscode.workspace.onDidCloseTextDocument(doc => matlabDiagnostics.delete(doc.uri))
 	);
 
 }
